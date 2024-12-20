f5e76081929ad280b279876b96da754d250583df
takanag <takanag@gmail.com>
2024-12-20 12:07:15 +0900

    Add files via upload

---
+++ gensen_select.php
@@ -0,0 +1,39 @@
+<?php
+// DB接続
+try {
+    $pdo = new PDO('mysql:dbname=db_gensen;charset=utf8;host=localhost', 'root', '');
+} catch (PDOException $e) {
+    exit('DBConnectError: ' . $e->getMessage());
+}
+
+// データ取得SQL
+$stmt = $pdo->prepare('SELECT * FROM gensen_master');
+$status = $stmt->execute();
+
+// 表形式のデータ作成
+$view = '';
+if ($status === false) {
+    $error = $stmt->errorInfo();
+    exit('ErrorQuery: ' . $error[2]);
+} else {
+    while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $view .= '<tr>';
+        // $view .= '<td>' . htmlspecialchars($result['place_id'], ENT_QUOTES, 'UTF-8') . '</td>';
+        $view .= '<td>' . htmlspecialchars($result['name'], ENT_QUOTES, 'UTF-8') . '</td>';
+        $view .= '<td>' . htmlspecialchars($result['review_summary'], ENT_QUOTES, 'UTF-8') . '</td>';
+        $view .= '<td>' . htmlspecialchars($result['facility_type'], ENT_QUOTES, 'UTF-8') . '</td>';
+        $view .= '<td>' . htmlspecialchars($result['address'], ENT_QUOTES, 'UTF-8') . '</td>';
+        $view .= '<td>' . htmlspecialchars($result['tel'], ENT_QUOTES, 'UTF-8') . '</td>';
+        $view .= '<td>' . htmlspecialchars($result['business_hours'], ENT_QUOTES, 'UTF-8') . '</td>';
+        $view .= '<td>' . htmlspecialchars($result['regular_holiday'], ENT_QUOTES, 'UTF-8') . '</td>';
+        $view .= '<td>' . htmlspecialchars($result['price'], ENT_QUOTES, 'UTF-8') . '</td>';
+        $view .= '<td>' . htmlspecialchars($result['spring_quality'], ENT_QUOTES, 'UTF-8') . '</td>';
+        $view .= '<td>' . htmlspecialchars($result['effect'], ENT_QUOTES, 'UTF-8') . '</td>';
+        $view .= '<td>' . htmlspecialchars($result['amenities'], ENT_QUOTES, 'UTF-8') . '</td>';
+        $view .= '<td>' . htmlspecialchars($result['facilities'], ENT_QUOTES, 'UTF-8') . '</td>';
+        $view .= '<td>' . htmlspecialchars($result['access'], ENT_QUOTES, 'UTF-8') . '</td>';
+        $view .= '<td>' . htmlspecialchars($result['other_info'], ENT_QUOTES, 'UTF-8') . '</td>';
+        $view .= '</tr>';
+    }
+}
+echo $view;
